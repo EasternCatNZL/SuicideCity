ef6ee57d7a9adce9e0fa53e21fcc3e4ec1b32ab3
EasternCatNZL <brianpwwong@gmail.com>
2017-08-24 17:00:59 +1200

    Photo stuff work

---
+++ SuicideCity/Assets/Scripts/Camera/PhotoBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoBehaviour : MonoBehaviour {
+
+    [Header("Interest objects list")]
+    [Tooltip("List of interest objects captured in the photo")]
+    public List<InterestBehaviour> interestObjectsList = new List<InterestBehaviour>();
+    //the main focus point of the photo
+    public InterestBehaviour mainFocusInterestObject;
+
+    //the image created when taking screenshot
+    private Sprite photo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //set the sprite of this photo
+    public void SetPhotoImage(Sprite newImage)
+    {
+        photo = newImage;
+    }
+
+    //add interest behaviour script details to the photo
+    public void AddInterestBehaviour(InterestBehaviour newInterestBehaviour)
+    {
+        interestObjectsList.Add(newInterestBehaviour);
+    }
+
+    //set the focus of this photo
+    public void SetFocusObject(InterestBehaviour focusBehaviour)
+    {
+        mainFocusInterestObject = focusBehaviour;
+    }
+}
---
+++ SuicideCity/Assets/Scripts/Interest/InterestBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterestBehaviour : MonoBehaviour {
+
+    [Header("Details of interest")]
+    [Tooltip("Name of this thing")]
+    public string interestName;
+    [Tooltip("Details of interest")]
+    public string[] interestDetailsArray = new string[0];
+
+	// Use this for initialization
+	void Start () {
+        //debug
+        //print(Camera.main.WorldToViewportPoint(transform.position));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
