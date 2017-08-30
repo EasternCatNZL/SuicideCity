a365abed3887bf4565494734d4ec83b56b05789c
Chimpster <chimpsterchip@gmail.com>
2017-08-31 10:26:00 +1200

    - Created a main menu - Adjusted props in the park

---
+++ SuicideCity/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadScene(string _SceneName)
+    {
+        InitiateFadeObject.Fade(_SceneName, Color.black, 2.0f);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
