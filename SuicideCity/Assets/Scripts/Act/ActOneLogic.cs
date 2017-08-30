e5020a2b135e19d31d46ef1625549de07a2bdfb0
EasternCatNZL <brianpwwong@gmail.com>
2017-08-30 17:04:03 +1200

    started act script

--- SuicideCity/Assets/Scripts/Act/ActOneLogic.cs
+++ SuicideCity/Assets/Scripts/Act/ActOneLogic.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class ActOneLogic : MonoBehaviour {
 
+    [Header("References to actors")]
+    [Tooltip("The victim in the act")]
+    public GameObject victimActor;
+    [Tooltip("The scene of the crime")]
+    public GameObject sceneObject;
+
+    [Header("Tags")]
+    [Tooltip("Tag for player")]
+    public string playerString = "Player";
+
+    private bool isStartAct = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +25,22 @@ public class ActOneLogic : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    //coroutine that runs after player has entered area
+    private IEnumerator ActCoroutine(){
+        //wait for flag to start
+        yield return isStartAct;
+
+
+    }
+
+    //start coroutine using trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        //if other is player, start coroutine
+        if (other.CompareTag(playerString))
+        {
+            isStartAct = true;
+        }
+    }
 }
